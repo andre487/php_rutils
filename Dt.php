c41f9e065c2013d0078bfebd5529d3c405d9b48b
Andre <andrey.prokopyuk@gmail.com>
2013-10-17 16:30:23 +0400

    add function Dt::distanceOfTimeInWords

--- Dt.php
+++ Dt.php
@@ -10,6 +10,9 @@ use php_rutils\struct\TimeParams;
  */
 class Dt
 {
+    const PREFIX_IN = "через"; //Day names (abbreviated, full, inflected, preposition)
+    const SUFFIX_AGO = "назад"; //Month names (abbreviated, full, inflected)
+
     private static $_DAY_NAMES = array(
         array('пн', 'понедельник', 'понедельник', "в\xa0"),
         array('вт', 'вторник', 'вторник', "во\xa0"),
@@ -18,7 +21,7 @@ class Dt
         array('пт', 'пятница', 'пятницу', "в\xa0"),
         array('сб', 'суббота', 'субботу', "в\xa0"),
         array('вск', 'воскресенье', 'воскресенье', "в\xa0")
-    ); //Day names (abbreviated, full, inflected, preposition)
+    ); //Day alternatives (i.e. one day ago -> yesterday)
 
     private static $_MONTH_NAMES = array(
         array("янв", "январь", "января"),
@@ -33,7 +36,16 @@ class Dt
         array("окт", "октябрь", "октября"),
         array("ноя", "ноябрь", "ноября"),
         array("дек", "декабрь", "декабря"),
-    ); //Month names (abbreviated, full, inflected)
+    ); //Forms (1, 2, 5) for noun 'day'
+
+    private static $_DAY_ALTERNATIVES = array(
+        1 => array("вчера", "завтра"),
+        2 => array("позавчера", "послезавтра"),
+    ); //Forms (1, 2, 5) for noun 'hour'
+
+    private static $_DAY_VARIANTS = array("день", "дня", "дней"); //Forms (1, 2, 5) for noun 'minute'
+    private static $_HOUR_VARIANTS = array("час", "часа", "часов"); //Prefix 'in' (i.e. B{in} three hours)
+    private static $_MINUTE_VARIANTS = array("минуту", "минуты", "минут"); //Prefix 'ago' (i.e. three hours B{ago})
 
     /**
      * Russian \DateTime::format
@@ -86,5 +98,149 @@ class Dt
 
         //Create date/time string
         return $params->date->format($params->format);
+    }
+
+    /**
+     * Represents distance of time in words
+     * @param string|int|\DateTime $fromTime Source time
+     * @param string|int|\DateTime $toTime Target time
+     * @param int $accuracy Level of accuracy (1..3), default=1
+     * @param string|\DateTimeZone|null $timeZone Time zone
+     * @throws \InvalidArgumentException
+     * @return string Distance of time in words
+     */
+    public function distanceOfTimeInWords($fromTime, $toTime = null, $accuracy = 1, $timeZone = null)
+    {
+        if ($accuracy < 1 || $accuracy > 3)
+            throw new \InvalidArgumentException('Wrong accuracy value (must be 1..3)');
+
+        /* @var $fromTime \DateTime */
+        /* @var $toTime \DateTime */
+        /* @var $toCurrent bool */
+        list($fromTime, $toTime, $toCurrent) = $this->_createFunctionParams($fromTime, $toTime, $timeZone);
+        $interval = $fromTime->diff($toTime);
+
+        $words = array();
+        $values = array();
+        $alternatives = array();
+        $this->_fillCollections($interval, $toCurrent, $words, $values, $alternatives);
+        $this->_trimArrays($words);
+        $this->_trimArrays($values);
+
+        $limit = min($accuracy, sizeof($words));
+        $realWords = array_slice($words, 0, $limit);
+        $realValues = array_slice($values, 0, $limit);
+
+        $length0 = sizeof($realValues);
+        $this->_trimArrays($realValues, $realWords);
+        $limit -= ($length0 - sizeof($realValues));
+
+        //if diff less than one minute
+        if ($interval->i == 0 && !$realWords) {
+            if ($interval->invert)
+                $zeroStr = 'менее чем через минуту';
+            else
+                $zeroStr = 'менее минуты назад';
+            return $zeroStr;
+        }
+
+        //if diff is 1 or 2 days
+        $days = $interval->d;
+        if ($toCurrent && $limit == 1 && ($days == 1 || $days == 2)) {
+            $altData = self::$_DAY_ALTERNATIVES[$days];
+            $altDay = ($interval->invert) ? $altData[1] : $altData[0];
+            return $altDay;
+        }
+
+        //general case
+        $realStr = implode(', ', $realWords);
+        if ($limit == 1 && $toCurrent && $alternatives)
+            $altStr = $alternatives[0];
+        else
+            $altStr = '';
+
+        $resultStr = $altStr ? $altStr : $realStr;
+        if ($interval->invert)
+            $resultStr = self::PREFIX_IN.' '.$resultStr;
+        else
+            $resultStr = $resultStr.' '.self::SUFFIX_AGO;
+
+        return $resultStr;
+    }
+
+    private function _createFunctionParams($fromTime, $toTime, $timeZone)
+    {
+        if (is_numeric($fromTime)) {
+            $timestamp = $fromTime;
+            $fromTime = new \DateTime();
+            $fromTime->setTimestamp($timestamp);
+        }
+        else if (is_string($fromTime)) {
+            $fromTime = new \DateTime($fromTime);
+        }
+
+        $toCurrent = false;
+        if ($toTime === null) {
+            $toTime = new \DateTime();
+            $toCurrent = true;
+        }
+        else if (is_numeric($toTime)) {
+            $timestamp = $toTime;
+            $toTime = new \DateTime();
+            $toTime->setTimestamp($timestamp);
+        }
+        else if (is_string($toTime)) {
+            $toTime = new \DateTime($toTime);
+        }
+
+        if (is_string($timeZone))
+            $timeZone = new \DateTimeZone($timeZone);
+
+        if ($timeZone) {
+            $fromTime->setTimezone($timeZone);
+            $toTime->setTimezone($timeZone);
+        }
+
+        return array($fromTime, $toTime, $toCurrent);
+    }
+
+    private function _fillCollections($interval, $toCurrent, &$words, &$values, &$alternatives)
+    {
+        $numeral = new Numeral();
+
+        $days = $interval->days;
+        $words[] = $numeral->getPlural($days, self::$_DAY_VARIANTS);
+        $values[] = $days;
+
+        $hours = $interval->h;
+        $words[] = $numeral->getPlural($hours, self::$_HOUR_VARIANTS);
+        $values[] = $hours;
+
+        if ($days == 0 && $hours == 1 && $toCurrent)
+            $alternatives[] = 'час';
+
+        $minutes = $interval->i;
+        $words[] = $numeral->getPlural($minutes, self::$_MINUTE_VARIANTS);
+        $values[] = $minutes;
+
+        if ($days == 0 && $hours == 0 && $minutes == 1 && $toCurrent)
+            $alternatives[] = 'минуту';
+    }
+
+    private function _trimArrays(array &$arr, array &$arr2=null)
+    {
+        $i = sizeof($arr) - 1;
+        while ($i >= 0 && $arr[$i] == 0) {
+            array_pop($arr);
+            if ($arr2)
+                array_pop($arr2);
+            --$i;
+        }
+
+        while (sizeof($arr) && $arr[0] == 0) {
+            array_shift($arr);
+            if ($arr2)
+                array_shift($arr2);
+        }
     }
 }
--- test/DtTest.php
+++ test/DtTest.php
@@ -99,4 +99,94 @@ class DtTest extends \PHPUnit_Framework_TestCase
             $this->assertEquals($expected, $strTime);
         }
     }
+
+    /**
+     * @covers \php_rutils\Dt::distanceOfTimeInWords
+     */
+    public function testDistanceOfTimeToCurrent()
+    {
+        $nowTime = strtotime('now');
+
+        $testData = array(
+            //past
+            date('Y-m-d H:i:s', $nowTime - 1) => 'менее минуты назад',
+            date('Y-m-d H:i:s', $nowTime - 60) => 'минуту назад',
+            date('Y-m-d H:i:s', $nowTime - 2*60) => '2 минуты назад',
+            date('Y-m-d H:i:s', $nowTime - 5*60) => '5 минут назад',
+            date('Y-m-d H:i:s', $nowTime - 60*60) => 'час назад',
+            date('Y-m-d H:i:s', $nowTime - 2*60*60) => '2 часа назад',
+            date('Y-m-d H:i:s', $nowTime - 5*60*60) => '5 часов назад',
+            date('Y-m-d H:i:s', $nowTime - 24*60*60) => 'вчера',
+            date('Y-m-d H:i:s', $nowTime - 2*24*60*60) => 'позавчера',
+            date('Y-m-d H:i:s', $nowTime - 3*24*60*60) => '3 дня назад',
+            date('Y-m-d H:i:s', $nowTime - 8*24*60*60 - 1*60*60) => '8 дней назад',
+            ($nowTime - 365*24*60*60) => '365 дней назад',
+
+            //future
+            date('Y-m-d H:i:s', $nowTime + 1) => 'менее чем через минуту',
+            date('Y-m-d H:i:s', $nowTime + 60) => 'через минуту',
+            date('Y-m-d H:i:s', $nowTime + 2*60) => 'через 2 минуты',
+            date('Y-m-d H:i:s', $nowTime + 5*60) => 'через 5 минут',
+            date('Y-m-d H:i:s', $nowTime + 60*60) => 'через час',
+            date('Y-m-d H:i:s', $nowTime + 2*60*60) => 'через 2 часа',
+            date('Y-m-d H:i:s', $nowTime + 5*60*60) => 'через 5 часов',
+            date('Y-m-d H:i:s', $nowTime + 24*60*60) => 'завтра',
+            date('Y-m-d H:i:s', $nowTime + 2*24*60*60) => 'послезавтра',
+            date('Y-m-d H:i:s', $nowTime + 3*24*60*60) => 'через 3 дня',
+            date('Y-m-d H:i:s', $nowTime + 8*24*60*60) => 'через 8 дней',
+            ($nowTime + 365*24*60*60) => 'через 365 дней',
+        );
+
+        foreach ($testData as $fromTime => $expected)
+            $this->assertEquals($expected, $this->_object->distanceOfTimeInWords($fromTime));
+
+        $fromTime = new \DateTime();
+        $fromTime->setTimestamp($nowTime + 365*24*60*60);
+        $this->assertEquals('через 365 дней', $this->_object->distanceOfTimeInWords($fromTime));
+    }
+
+    /**
+     * @covers \php_rutils\Dt::distanceOfTimeInWords
+     */
+    public function testDistanceOfTimeToTime()
+    {
+        $nowTimeStamp = strtotime('now');
+        $fromTimeStamp = $nowTimeStamp + 365*24*60*60;
+
+        $timeZone = new \DateTimeZone('Europe/London');
+        $toTime = new \DateTime('now', $timeZone);
+
+        $fromTime = new \DateTime('now');
+        $fromTime->setTimestamp($fromTimeStamp);
+
+        $expected = 'через 365 дней';
+        $this->assertEquals(
+            $expected, $this->_object->distanceOfTimeInWords($fromTimeStamp, $nowTimeStamp, 1, $timeZone)
+        );
+        $this->assertEquals($expected, $this->_object->distanceOfTimeInWords($fromTime, $toTime, 1, $timeZone));
+    }
+
+    /**
+     * @covers \php_rutils\Dt::distanceOfTimeInWords
+     */
+    public function testDistanceOfTimeAccuracy()
+    {
+        $nowTime = strtotime('now');
+        $futureTime = $nowTime + 364*24*60*60 + 3*60*60 + 4*60;
+        $pastTime = $nowTime - 364*24*60*60 - 3*60*60 - 4*60;
+
+        //future
+        $this->assertEquals('через 364 дня', $this->_object->distanceOfTimeInWords($futureTime, $nowTime, 1));
+        $this->assertEquals('через 364 дня, 3 часа', $this->_object->distanceOfTimeInWords($futureTime, $nowTime, 2));
+        $this->assertEquals(
+            'через 364 дня, 3 часа, 4 минуты', $this->_object->distanceOfTimeInWords($futureTime, $nowTime, 3)
+        );
+
+        //past
+        $this->assertEquals('364 дня назад', $this->_object->distanceOfTimeInWords($pastTime, $nowTime, 1));
+        $this->assertEquals('364 дня, 3 часа назад', $this->_object->distanceOfTimeInWords($pastTime, $nowTime, 2));
+        $this->assertEquals(
+            '364 дня, 3 часа, 4 минуты назад', $this->_object->distanceOfTimeInWords($pastTime, $nowTime, 3)
+        );
+    }
 }
