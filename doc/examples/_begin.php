cdde9b25f82ffea981a348575cc558518e4a7237
Andre <andrey.prokopyuk@gmail.com>
2013-10-23 13:51:46 +0400

    fix examples web-representation

--- doc/examples/_begin.php
+++ doc/examples/_begin.php
@@ -5,6 +5,9 @@ define('LIB_DIR', realpath(__DIR__.'/../../..'));
 define('CLI', php_sapi_name() == 'cli');
 mb_internal_encoding('UTF-8');
 
+if (CLI == false)
+    header('Content-type: text/plain; charset=UTF-8');
+
 spl_autoload_register(function($className) {
         $classPath = LIB_DIR. DIRECTORY_SEPARATOR . str_replace('\\', DIRECTORY_SEPARATOR, $className).'.php';
         if (is_file($classPath))
