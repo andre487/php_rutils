a9dc40d546244940fbbb942b494bedb4b7c04e58
Andre <andrey.prokopyuk@gmail.com>
2013-10-23 17:21:35 +0400

    fix tests to match new translation table

--- test/TranslitTest.php
+++ test/TranslitTest.php
@@ -25,12 +25,12 @@ class TranslitTest extends \PHPUnit_Framework_TestCase
             'тест №1' => 'test #1',
             'проверка' => 'proverka',
             'транслит' => 'translit',
-            'правда ли это' => 'pravda li eto',
-            'Щука' => 'Schuka',
+            'правда ли это' => 'pravda li jeto',
+            'Щука' => 'SHCHuka',
             '«Вот так вот»' => '"Vot tak vot"',
             '‘Или вот так’' => "'Ili vot tak'",
             '– Да…' => '- Da...',
-            'Двигатель “Pratt&Whitney”' => 'Dvigatel\' "Pratt&Whitney"'
+            'Двигатель “Pratt&Whitney”' => 'Dvigatel "Pratt&Whitney"'
         );
         foreach ($testData as $testValue => $expected)
             $this->assertEquals($expected, $this->_object->translify($testValue));
@@ -45,8 +45,7 @@ class TranslitTest extends \PHPUnit_Framework_TestCase
             'test #1' => 'тест №1',
             'proverka' => 'проверка',
             'translit' => 'транслит',
-            'Schuka' => 'Щука',
-            'SCHuka' => 'Щука',
+            'SHCHuka' => 'Щука',
             '- Da...' => '– Да…',
         );
         foreach ($testData as $testValue => $expected)
@@ -62,13 +61,13 @@ class TranslitTest extends \PHPUnit_Framework_TestCase
             'тест' => 'test',
             'Проверка связи' => 'proverka-svyazi',
             'me&you' => 'me-and-you',
-            'и еще один тест' => 'i-esche-odin-test',
+            'и еще один тест' => 'i-eshche-odin-test',
             'Проверка связи…' => 'proverka-svyazi',
             "Проверка\x0aсвязи 2" => 'proverka-svyazi-2',
             "Проверка\201связи 3" => 'proverkasvyazi-3',
             'World of Warcraft' => 'world-of-warcraft',
-            'Юнит-тесты — наше всё' => 'yunit-testyi---nashe-vsyo',
-            'Юнит-тесты ‒ наше всё' => 'yunit-testyi---nashe-vsyo',
+            'Юнит-тесты — наше всё' => 'yunit-testy---nashe-vsjo',
+            'Юнит-тесты ‒ наше всё' => 'yunit-testy---nashe-vsjo',
             '95−34' => '95-34',
         );
         foreach ($testData as $testValue => $expected)
