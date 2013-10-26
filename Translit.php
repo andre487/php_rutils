2c177cf263039b82a1e34211e14d8e93c156795c
Andre <andrey.prokopyuk@gmail.com>
2013-10-26 18:05:03 +0400

    transliteration table now matches ISO9, Translit::slugify improved

--- Translit.php
+++ Translit.php
@@ -9,57 +9,61 @@ namespace php_rutils;
 class Translit
 {
     private static $_TRANSLATION_TABLE = array(
-        array("'", "'"),
-        array('"', '"'),
+        //Non-alphabet symbols
         array("‘", "'"),
         array("’", "'"),
         array("«", '"'),
         array("»", '"'),
         array("“", '"'),
         array("”", '"'),
-        array("–", "-"), // en dash
-        array("—", "-"), // em dash
-        array("‒", "-"), // figure dash
-        array("−", "-"), // minus
-        array("…", "..."),
         array("№", "#"),
-        // three-symbols replacements
-        array('э', 'je'), array('ё', 'jo'), array('я', 'ya'), array('ю', 'yu'), array('ы', 'y'), array('ж', 'zh'),
-        array('й', 'y'), array('щ', 'shch'), array('ч', 'ch'), array('ш', 'sh'), array('э', 'ea'), array('а', 'a'),
-        array('б', 'b'), array('в', 'v'), array('г', 'g'), array('д', 'd'), array('е', 'e'), array('з', 'z'),
-        array('и', 'i'), array('к', 'k'), array('л', 'l'), array('м', 'm'), array('н', 'n'), array('о', 'o'),
-        array('п', 'p'), array('р', 'r'), array('с', 's'), array('т', 't'), array('у', 'u'), array('ф', 'f'),
-        array('х', 'h'), array('ц', 'c'), array('э', 'e'), array('ь', ''), array('ъ', ''), array('й', 'y'),
 
-        array('Э', 'JE'), array('Ё', 'JO'), array('Я', 'YA'), array('Ю', 'YU'), array('Ы', 'Y'), array('Ж', 'ZH'),
-        array('Й', 'Y'), array('Щ', 'SHCH'), array('Ч', 'CH'), array('Ш', 'SH'), array('Э', 'E'), array('А', 'A'),
-        array('Б', 'B'), array('В', 'V'), array('Г', 'G'), array('Д', 'D'), array('Е', 'E'), array('З', 'Z'),
-        array('И', 'I'), array('К', 'K'), array('Л', 'L'), array('М', 'M'), array('Н', 'N'), array('О', 'O'),
-        array('П', 'P'), array('Р', 'R'), array('С', 'S'), array('Т', 'T'), array('У', 'U'), array('Ф', 'F'),
-        array('Х', 'H'), array('Ц', 'C'), array('Э', 'E'), array('Ь', ''), array('Ъ', ''), array('Й', 'Y'),
-        // Make english alphabet full: append english-english pairs
-        // for symbols which is not used in russian-english
-        // translations. Used in slugify.
-        array("c", "c"),
-        array("q", "q"),
-        array("y", "y"),
-        array("x", "x"),
-        array("w", "w"),
-        array("1", "1"),
-        array("2", "2"),
-        array("3", "3"),
-        array("4", "4"),
-        array("5", "5"),
-        array("6", "6"),
-        array("7", "7"),
-        array("8", "8"),
-        array("9", "9"),
-        array("0", "0"),
+        //Alphabet (ISO9 [ГОСТ 7.79—2000], Scheme B)
+        //3-symbolic
+        array('Щ', 'Shh'), array('щ', 'shh'),
+
+        //2-symbolic
+        array('Ё', 'Yo'), array('ё', 'yo'),
+        array('Ж', 'Zh'), array('ж', 'zh'),
+        array('Ц', 'Cz'), array('ц', 'cz'),
+        array('Ч', 'Ch'), array('ч', 'ch'),
+        array('Ш', 'Sh'), array('ш', 'sh'),
+        array('ъ', '``'), array('Ъ', '``'),
+        array('Ы', 'Y`'), array('ы', 'y`'),
+        array('Э', 'E`'), array('э', 'e`'),
+        array('Ю', 'Yu'), array('ю', 'yu'),
+        array('Я', 'Ya'), array('я', 'ya'),
+
+        //1-symbolic
+        array('А', 'A'), array('а', 'a'),
+        array('Б', 'B'), array('б', 'b'),
+        array('В', 'V'), array('в', 'v'),
+        array('Г', 'G'), array('г', 'g'),
+        array('Д', 'D'), array('д', 'd'),
+        array('Е', 'E'), array('е', 'e'),
+        array('З', 'Z'), array('з', 'z'),
+        array('И', 'I'), array('и', 'i'),
+        array('Й', 'J'), array('й', 'j'),
+        array('К', 'K'), array('к', 'k'),
+        array('Л', 'L'), array('л', 'l'),
+        array('М', 'M'), array('м', 'm'),
+        array('Н', 'N'), array('н', 'n'),
+        array('О', 'O'), array('о', 'o'),
+        array('П', 'P'), array('п', 'p'),
+        array('Р', 'R'), array('р', 'r'),
+        array('С', 'S'), array('с', 's'),
+        array('Т', 'T'), array('т', 't'),
+        array('У', 'U'), array('у', 'u'),
+        array('Ф', 'F'), array('ф', 'f'),
+        array('Х', 'X'), array('х', 'x'),
+        array('ь', '`'), array('Ь', '`'),
     );  //Translation table
 
     private static $_RU_ALPHABET;
     private static $_EN_ALPHABET;
-    private static $_ALPHABET;
+
+    private static $_CORRECTION_PATTERN = array('#(\w)«#u', '#(\w)“#u', '#(\w)‘#u');
+    private static $_CORRECTION_REPLACEMENT = array('$1»', '$1”', '$1’');
 
     /**
      * "Constructor" for class variables
@@ -92,28 +96,25 @@ class Translit
      */
     public function detranslify($inString)
     {
-        return str_replace(self::$_EN_ALPHABET, self::$_RU_ALPHABET, $inString);
+        $dirtyResult = str_replace(self::$_EN_ALPHABET, self::$_RU_ALPHABET, $inString);
+        return preg_replace(self::$_CORRECTION_PATTERN, self::$_CORRECTION_REPLACEMENT, $dirtyResult);
     }
 
     /**
      * Prepare string for slug (i.e. URL or file/dir name)
      * @param string $inString Input string
-     * @param string|null $encoding
      * @return string Slug-string
      */
-    public function slugify($inString, $encoding=null)
+    public function slugify($inString)
     {
-        if ($encoding === null)
-            $encoding = RUtils::$encoding;
-        $inString = mb_strtolower($inString, $encoding);
-
         //convert & to "and"
         $inString = preg_replace('/(?:&amp;)|&/u' , ' and ', $inString);
-        // replace spaces by hyphen
-        $inString = preg_replace('/[-\s\t]+/u', '-', $inString);
 
-        $translitString = $this->translify($inString);
-        return preg_replace('/[^\w-]+/', '', $translitString);
+        //replace spaces
+        $inString = preg_replace('/[—−-\s\t]+/u', '-', $inString);
+
+        $translitString = strtolower($this->translify($inString));
+        return preg_replace('/[^a-z0-9_-]+/i', '', $translitString);
     }
 }
 
--- test/TranslitTest.php
+++ test/TranslitTest.php
@@ -25,12 +25,11 @@ class TranslitTest extends \PHPUnit_Framework_TestCase
             'тест №1' => 'test #1',
             'проверка' => 'proverka',
             'транслит' => 'translit',
-            'правда ли это' => 'pravda li jeto',
-            'Щука' => 'SHCHuka',
+            'правда ли это' => 'pravda li e`to',
+            'Щука' => 'Shhuka',
             '«Вот так вот»' => '"Vot tak vot"',
             '‘Или вот так’' => "'Ili vot tak'",
-            '– Да…' => '- Da...',
-            'Двигатель “Pratt&Whitney”' => 'Dvigatel "Pratt&Whitney"'
+            'Двигатель “Pratt&Whitney”' => 'Dvigatel` "Pratt&Whitney"'
         );
         foreach ($testData as $testValue => $expected)
             $this->assertEquals($expected, $this->_object->translify($testValue));
@@ -45,8 +44,10 @@ class TranslitTest extends \PHPUnit_Framework_TestCase
             'test #1' => 'тест №1',
             'proverka' => 'проверка',
             'translit' => 'транслит',
-            'SHCHuka' => 'Щука',
-            '- Da...' => '– Да…',
+            'pravda li e`to' => 'правда ли это',
+            'Shhuka' => 'Щука',
+            '"Vot tak vot"' => '«Вот так вот»',
+            "'Ili vot tak'" => '‘Или вот так’',
         );
         foreach ($testData as $testValue => $expected)
             $this->assertEquals($expected, $this->_object->detranslify($testValue));
@@ -61,16 +62,30 @@ class TranslitTest extends \PHPUnit_Framework_TestCase
             'тест' => 'test',
             'Проверка связи' => 'proverka-svyazi',
             'me&you' => 'me-and-you',
-            'и еще один тест' => 'i-eshche-odin-test',
+            'и еще один тест' => 'i-eshhe-odin-test',
             'Проверка связи…' => 'proverka-svyazi',
             "Проверка\x0aсвязи 2" => 'proverka-svyazi-2',
-            "Проверка\201связи 3" => 'proverkasvyazi-3',
             'World of Warcraft' => 'world-of-warcraft',
-            'Юнит-тесты — наше всё' => 'yunit-testy---nashe-vsjo',
-            'Юнит-тесты ‒ наше всё' => 'yunit-testy---nashe-vsjo',
+            'Юнит-тесты - наше всё' => 'yunit-testy-nashe-vsyo',
+            'Юнит-тесты — наше всё' => 'yunit-testy-nashe-vsyo',
             '95−34' => '95-34',
+            'Эмгыр вар Эмрейс тоже хочет быть в slugify' => 'emgyr-var-emrejs-tozhe-xochet-byt-v-slugify',
         );
         foreach ($testData as $testValue => $expected)
             $this->assertEquals($expected, $this->_object->slugify($testValue));
+    }
+
+    /**
+     * @covers \php_rutils\Translit::translify
+     * @covers \php_rutils\Translit::detranslify
+     */
+    public function testUniqueness()
+    {
+        $originalText = file_get_contents(__DIR__.'/data/zarathustra.txt');
+
+        $translitText = $this->_object->translify($originalText);
+        $cyrText = $this->_object->detranslify($translitText);
+
+        $this->assertEquals($originalText, $cyrText);
     }
 }
